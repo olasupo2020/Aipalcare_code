cc0428c29441a9bbb8113c961ca2d92e20803fa6
aipalbot <info@aipalbot.com>
2021-03-06 16:18:30 -0500

    remaining changes

--- src/forms/ServiceProviderForm.js
+++ src/forms/ServiceProviderForm.js
@@ -30,13 +30,13 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const initData = {
-  id:1,
+  id: 1,
   firstName: '',
   lastName: '',
   nhpn: '',
   govtBody: '',
   credential: '',
-  year:'',
+  year: '',
   licenseNo: '',
   date: '',
   divisionNames: '',
@@ -59,62 +59,74 @@ export default function ServiceProviderForm() {
   const [selectedDate, setSelectedDate] = useState(new Date(defaultDate));
   const [isError, setIsError] = useState(true);
   const [errorMessage, setErrorMessage] = useState('');
-  const [error, setError] = useState(false);
+  const currentDate = new Date();
 
-  const currentDate = initData.todayDate;
+
+
+  const validate = (fieldValues = values, defaultDate) => {
+    let temp = { ...errors }
+    if ('firstName' in fieldValues)
+      temp.firstName = fieldValues.firstName ? "" : "This field is required."
+    if ('lastName' in fieldValues)
+      temp.lastName = fieldValues.lastName ? "" : "This field is required."
+    if ('nhpn' in fieldValues)
+      temp.nhpn = fieldValues.nhpn ? "" : "This field is required."
+    if ('govtBody' in fieldValues)
+      temp.govtBody = fieldValues.govtBody ? "" : "This field is required."
+    if ('credential' in fieldValues)
+      temp.credential = fieldValues.credential ? "" : "This field is required."
+    if ('year' in fieldValues)
+      temp.year = fieldValues.year ? "" : "This field is required."
+    /*if ('email' in fieldValues)
+        temp.email = (/$^|.+@.+..+/).test(fieldValues.email) ? "" : "Email is not valid."*/
+    if ('licenseNo' in fieldValues)
+      temp.licenseNo = fieldValues.licenseNo.length > 9 ? "" : "Minimum 10 numbers required."
+    if ('divisionNames' in fieldValues)
+      temp.divisionNames = fieldValues.divisionNames.length != 0 ? "" : "This field is required."
+    if ('date' in fieldValues)
+      temp.date = fieldValues.date.length != 0 || fieldValues.date == defaultDate ? "" : "This field is required."
+    setErrors({
+      ...temp
+    })
+
+    if (fieldValues == values)
+      return Object.values(temp).every(err => err == "")
+
+  }
+
 
   const handleDateChange = (date) => {
+
     if (date > currentDate) {
-      setErrorMessage("Invalid Date");
+      setErrors({ date: "Invalid Date" });
       setSelectedDate(date);
-      setError(true);
+
     } else {
       setSelectedDate(date);
-      setErrorMessage("");
-      setError(false);
-    }
-  };
-
-
-
-  const handleSubmitForm = (event) => {
-    if (!error) {
-      axios.post('https://jsonplaceholder.typicode.com/posts', values)
-        .then(response => {
-          console.log(response)
-        })
-        .catch(error => {
-          console.log(error)
-        })
-      console.log(values);
-    }
-    else {
-      console.log("There is error in your input");
+      values.date = date;
+      setErrors({ date: "" });
+      setIsError(false);
     }
   }
-};
+
+  const {
+    values,
+    setValues,
+    errors,
+    setErrors,
+    handleInputChange,
+  } = UseForm(initData, true, validate);
 
 
-const {
-  values,
-  setValues,
-  errors,
-  setErrors,
-  handleInputChange
-} = UseForm(initData, true, validate);
-
-
-const handleSubmitForm  = e => {
+  const handleSubmitForm = e => {
     e.preventDefault()
     console.log(isError);
     console.log(validate())
-    if (validate() && !isError){
+    if (validate() && !isError) {
       console.log(values);
-     
+
     }
-}
-
-
+  }
 
   const ITEM_HEIGHT = 48;
   const ITEM_PADDING_TOP = 8;
@@ -127,66 +139,71 @@ const handleSubmitForm  = e => {
     },
   };
 
-  
+
   return (
-    
+
     <Form title={CaasData.formTitle} onSubmit={handleSubmitForm}>
       <Grid container spacing={3}>
         <Grid item xs={12} >
-        <TextField
+          <TextField
             label={CaasData.firstName}
             id="mui-theme-provider-standard-input"
             name="firstName"
-            value={values.firstName}
             style={{ width: '60%' }}
             onChange={handleInputChange}
             error={Boolean(errors?.firstName)}
             helperText={errors?.firstName}
           />
+          <div>
+          </div>
         </Grid>
         <Grid item xs={12} >
           <TextField
-             error={Boolean(errors?.lastName)}
-             helperText={errors?.lastName}
+            error={Boolean(errors?.lastName)}
+            helperText={errors?.lastName}
             label={CaasData.lastName}
 
             id="mui-theme-provider-outlined-input"
             name="lastName"
-            value={values.lastName}
             style={{ width: '60%' }}
             onChange={handleInputChange}
           />
         </Grid>
 
         <Grid item xs={12} >
-          <FormControl style={{ width: '100%', margin: '0 20%' }} >
-            <InputLabel id="demo-mutiple-name-label">Select</InputLabel>
+          <FormControl style={{ width: '100%', margin: '0 20%' }}
+            error={Boolean(errors?.divisionNames)}
+          >
+            <InputLabel id="demo-mutiple-name-label">{CaasData.specialization}</InputLabel>
             <Select
 
+
+              name="divisionNames"
               labelId="demo-simple-select-outlined-label"
               id="demo-simple-select-outlined"
-              onChange={handleChange}
+              onChange={handleInputChange}
               input={<Input />}
               style={{ width: '60%' }}
-
             >
-              {names.map((name) => (
+              {divisionNames.map((name) => (
                 <MenuItem key={name} value={name} >
                   <ListItemText primary={name} />
                 </MenuItem>
               ))}
             </Select>
+            <FormHelperText>{errors?.divisionNames}</FormHelperText>
           </FormControl>
+
+
 
         </Grid>
         <Grid item xs={12} >
           <TextField
-             error={Boolean(errors?.nhpn)}
-             helperText={errors?.nhpn}
+            error={Boolean(errors?.nhpn)}
+            helperText={errors?.nhpn}
             label={CaasData.nhpn}
             id="mui-theme-provider-outlined-input"
             name="nhpn"
-            value={values.nhpn}
             style={{ width: '60%' }}
             onChange={handleInputChange}
           />
@@ -194,36 +211,33 @@ const handleSubmitForm  = e => {
 
         <Grid item xs={12}>
           <TextField
-               error={Boolean(errors?.licenseNo)}
-               helperText={errors?.licenseNo}
+            error={Boolean(errors?.licenseNo)}
+            helperText={errors?.licenseNo}
             label={CaasData.licenseNo}
             id="mui-theme-provider-outlined-input"
             name="licenseNo"
-            value={values.licenseNo}
             style={{ width: '60%' }}
             onChange={handleInputChange}
           /></Grid>
 
         <Grid item xs={12} >
           <TextField
-             error={Boolean(errors?.govtBody)}
-             helperText={errors?.govtBody}
+            error={Boolean(errors?.govtBody)}
+            helperText={errors?.govtBody}
             label={CaasData.govtBody}
             id="mui-theme-provider-outlined-input"
             name="govtBody"
-            value={values.govtBody}
             style={{ width: '60%' }}
             onChange={handleInputChange}
           />
         </Grid>
         <Grid item xs={12} >
           <TextField
-              error={Boolean(errors?.credential)}
-              helperText={errors?.credential}
+            error={Boolean(errors?.credential)}
+            helperText={errors?.credential}
             label={CaasData.credential}
             id="mui-theme-provider-outlined-input"
             name="credential"
-            value={values.setCredential}
             style={{ width: '60%' }}
             onChange={handleInputChange}
           /></Grid>
@@ -234,14 +248,14 @@ const handleSubmitForm  = e => {
             label={CaasData.year}
             id="mui-theme-provider-outlined-input"
             name="year"
-            value={values.year}
             style={{ width: '60%' }}
             onChange={handleInputChange}
           /></Grid>
         <Grid item xs={12}>
-          <MuiPickersUtilsProvider utils={DateFnsUtils}>
+          <MuiPickersUtilsProvider utils={DateFnsUtils} >
             <KeyboardDatePicker
-
+              error={Boolean(errors?.date)}
+              helperText={errors?.date}
               format="MM/dd/yyyy"
               margin="normal"
               id="date-picker-inline"
@@ -255,13 +269,15 @@ const handleSubmitForm  = e => {
 
             />
 
+
           </MuiPickersUtilsProvider>
-          <div style={{ color: 'red' }}><span>{errorMessage}</span></div>
+
+
         </Grid>
         <Grid item xs={12} >
           <br />
           <div className={classes.root}>
-            <Button variant="contained" color="primary" startIcon={<CloudUploadIcon />}  type="submit" onClick={handleSubmitForm}>
+            <Button variant="contained" color="primary" startIcon={<CloudUploadIcon />} type="submit" onClick={handleSubmitForm}>
               {CaasData.saveButton}
             </Button>
             <Button variant="contained" color="primary" startIcon={<EditIcon />} >
